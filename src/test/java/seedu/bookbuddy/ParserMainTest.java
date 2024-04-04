428e620a5d8d217b8862616a27e6b8c52733d698
Yeo Zong Yao <yeozongyao@gmail.com>
2024-04-05 00:27:06 +0800

    Checkstyle fixes

--- src/test/java/seedu/bookbuddy/ParserMainTest.java
+++ src/test/java/seedu/bookbuddy/ParserMainTest.java
@@ -17,7 +17,9 @@ import java.io.ByteArrayOutputStream;
 import java.io.InputStream;
 import java.io.PrintStream;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 
 public class ParserMainTest {
