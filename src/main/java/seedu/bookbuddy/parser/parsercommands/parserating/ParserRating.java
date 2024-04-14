0ba1a3694c97586c3a93054f8186f57ea3e0d1ab
Yeo Zong Yao <yeozongyao@gmail.com>
2024-04-15 04:55:42 +0800

    Bug fix - Exception message should be more accurate

--- src/main/java/seedu/bookbuddy/parser/parsercommands/parserating/ParserRating.java
+++ src/main/java/seedu/bookbuddy/parser/parsercommands/parserating/ParserRating.java
@@ -10,7 +10,7 @@ public class ParserRating {
         int rating;
         assert inputArray.length >= 2 : "Command requires additional arguments";
         Exceptions.validateCommandArguments(inputArray, 2, "The rating " +
-                "command requires a book index.");
+                "command requires a book index and a rating.");
         String[] ratingParts = RatingChecks.getRatingParts(inputArray);
         index = RatingChecks.parseInteger(ratingParts[0],  ratingParts[0] + " is not a valid integer");
         rating = RatingChecks.parseInteger(ratingParts[1], ratingParts[1] + " is not a valid integer");
