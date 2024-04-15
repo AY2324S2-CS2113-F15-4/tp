e7032743e08fa220f53ba4625a45f5f3013f5a7e
Yeo Zong Yao <yeozongyao@gmail.com>
2024-04-15 18:00:53 +0800

    Bug fix - added accurate exception messages

--- src/main/java/seedu/bookbuddy/parser/parsercommands/parsegenre/InputLooper.java
+++ src/main/java/seedu/bookbuddy/parser/parsercommands/parsegenre/InputLooper.java
@@ -4,6 +4,7 @@ import exceptions.InvalidInputException;
 import seedu.bookbuddy.BookBuddy;
 import seedu.bookbuddy.Ui;
 import seedu.bookbuddy.booklist.BookList;
+import seedu.bookbuddy.parser.parsercommands.parserating.RatingChecks;
 
 import java.io.IOException;
 import java.util.Objects;
@@ -22,7 +23,7 @@ public class InputLooper {
      * @return The final valid input from the user.
      * @throws IOException If an I/O error occurs while handling the input.
      */
-    public String inputLooper(String input, Scanner scanner, BookList books) throws IOException {
+    public String  inputLooper(String input, Scanner scanner, BookList books) throws IOException {
         while (input == null) {
             try {
                 input = handleInitialInput(scanner);
@@ -58,7 +59,7 @@ public class InputLooper {
                 Ui.printSingleIndentation();
             }
             if (indentationValue == 2) {
-                System.out.println("Enter the number for the desired genre, or add a new one:");
+                System.out.println("Enter the number for the desired genre, or add a new one, or 'exit' to go back:");
                 Ui.printSingleIndentation();
             }
             return true;
@@ -162,30 +163,33 @@ public class InputLooper {
      */
     String processSelection(String newInput, Scanner scanner, BookList books)
             throws InvalidInputException, IOException {
-        int selection = Integer.parseInt(newInput);
-        if (selection == books.genreList.getAvailableGenres().size() + 1) {
-            Ui.printDoubleIndentation();
-            System.out.print("Enter the new genre:\n");
-            Ui.printDoubleIndentation();
-            String genre = handleSecondaryInput(scanner);
-            return NewGenreModifier.duplicateChecker(genre, books);
-        }
-
-        if (selection > 0 && selection <= books.genreList.getAvailableGenres().size()) {
-            return books.genreList.getAvailableGenres().get(selection - 1);
-        }
-
-        if (selection > books.genreList.getAvailableGenres().size() + 1) {
-            System.out.println("That's not within the options man... try again.. or 'exit' to go back " +
-                    "or 'bye' to close program");
-            Ui.printSingleIndentation();
-            return null;
-        }
+        int selection = 0;
         try {
-            throw new InvalidInputException(selection + " is an invalid selection. Please enter a " +
-                    "valid number or type 'exit' to go back");
-        } catch (InvalidInputException e) {
-            System.out.println(selection + " is not a valid integer man.. try again.. or 'exit' to go back");
+            //int selection = Integer.parseInt(newInput);
+            selection = RatingChecks.parseInteger(newInput, newInput + "'s too massive a number.. " +
+                    "You're messing with me");
+            if (selection == books.genreList.getAvailableGenres().size() + 1) {
+                Ui.printDoubleIndentation();
+                System.out.print("Enter the new genre:\n");
+                Ui.printDoubleIndentation();
+                String genre = handleSecondaryInput(scanner);
+                return NewGenreModifier.duplicateChecker(genre, books);
+            }
+            if (selection > 0 && selection <= books.genreList.getAvailableGenres().size()) {
+                return books.genreList.getAvailableGenres().get(selection - 1);
+            }
+            if (selection > books.genreList.getAvailableGenres().size() + 1 && selection < 999) {
+                System.out.println("That's not within the options man... try again.. or 'exit' to go back " +
+                        "or 'bye' to close program");
+                Ui.printSingleIndentation();
+                return null;
+            }
+            if (selection <= 0) {
+                throw new InvalidInputException("Did you not see that the lowest option is 1??");
+            }
+            throw new InvalidInputException(selection + "'s too big.. You're messing with me");
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
             Ui.printSingleIndentation();
             return null;
         }
